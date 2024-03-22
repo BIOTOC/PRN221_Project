9466cc2d86504f1e9c5bbae61c32d3c9c1265c8e
BIOTOC <phongdb05@gmail.com>
2024-03-22 19:34:00 +0700

    update UI

---
+++ PRN221_Project/Pages/Classes/Management.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRN221_Project.Pages.Classes
+{
+    public class ManagementModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- PRN221_Project/Program.cs
+++ PRN221_Project/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+//builder.Services.AddDbContext<MoviesContext>(
+//    opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("MyConStr"))
+//    );
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapRazorPages();
 
 app.Run();
